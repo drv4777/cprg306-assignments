32536e465aa302f1a42651b257df4613c2c72b0e
drv4777 <diegoriveravazquez47@gmail.com>
2025-09-11 09:20:56 -0600

    Assignment 2 Commit

---
+++ app/week-2/page.js
@@ -0,0 +1,10 @@
+import StudentInfo from "./student-info";
+export default function Page()
+{
+    return(
+        <main>
+            <h1 className="text-3xl text-blue-400 bold-font">Shopping List</h1>
+            <StudentInfo />
+        </main>
+    );
+}
---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+export default function StudentInfo()
+{
+    return (
+    <main>
+        <p>Diego Rivera</p>
+        <Link href="https://github.com/drv4777/cprg306-assignments.git">Github Repo Link</Link>
+    </main>
+    );
+}
